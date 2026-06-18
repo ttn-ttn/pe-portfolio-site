9748d31219513f9ccb7a2d4b2f9cc0cd8ca1451e
Tomas Ortin Fernandez (quanrong) <quanrong@mailbox.org>
2026-06-18 16:38:10 +0200

    feat: inject bio using yaml content file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
@@ -9,3 +10,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(base_dir, "../content.yaml")) as f:
+    content = yaml.safe_load(f)
+
+@app.context_processor
+def inject_content():
+    return content
