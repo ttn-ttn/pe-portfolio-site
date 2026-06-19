bea5626cd7cc60054bfb859e1852d2d6d9fc016d
Tomas Ortin Fernandez (quanrong) <quanrong@mailbox.org>
2026-06-19 13:46:51 +0200

    feat: add travel map

---
+++ app/static/js/travel.js
@@ -0,0 +1,14 @@
+// The following setup code is based in the tutorial at
+// https://leafletjs.com/examples/quick-start/
+let map = L.map('map').setView([41.505, 0], 1.8);
+L.tileLayer(
+  'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+export function markLocations(locations) {
+  locations.forEach(loc => {
+    L.marker(loc).addTo(map);
+  });
+}
