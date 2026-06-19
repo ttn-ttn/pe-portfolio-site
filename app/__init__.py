5ff78ac285e6243ca50917ae91978902bb98767b
Tomas Ortin Fernandez (quanrong) <quanrong@mailbox.org>
2026-06-19 11:15:18 +0200

    feat: add dynamically generated pages and navigation

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
-
 base_dir = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(base_dir, "../content.yaml")) as f:
     content = yaml.safe_load(f)
@@ -18,3 +13,14 @@ with open(os.path.join(base_dir, "../content.yaml")) as f:
 @app.context_processor
 def inject_content():
     return content
+
+for page in content['nav']:
+    url = page['url']
+    template = page['template']
+    endpoint = url.strip('/') or 'index'
+
+    app.add_url_rule(
+        url,
+        endpoint,
+        lambda t=template: render_template(t, title=page['name'])
+    )
