095c42a2452fa90d2819d47ab97a1672aa02dfb3
Tomas Ortin Fernandez (quanrong) <quanrong@mailbox.org>
2026-07-07 15:34:14 +0200

    X: add database

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,18 @@ import os
 import yaml
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import *
 
 load_dotenv()
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+                     user=os.getenv("MYSQL_USER"),
+                     password=os.getenv("MYSQL_PASSWORD"),
+                     host=os.getenv("MYSQL_HOST"),
+                     port=3306)
+
+print(mydb)
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 content_path = os.getenv("CONTENT", "content.yaml")
